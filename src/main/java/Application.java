a3c136b69df95456a42e21980b2d665cc74d1187
Luis Pires <luispires310500@gmail.com>
2024-12-23 02:12:29 +0000

    Added comments explaining the train of thought behind the developed solution to the Java files.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -13,35 +13,42 @@ import jakarta.ws.rs.core.Response;
 
 @Path("/")
 public class Application {
-    private static Hashtable<Integer,BigInteger> values_dictionary = new Hashtable<Integer,BigInteger>();
+    private static Hashtable<Integer,BigInteger> values_dictionary = new Hashtable<Integer,BigInteger>(); //creating an Hashtable to save values of intermediate calculations for future use;
     
     @Inject
-    Functions labseq_funct;
+    Functions labseq_funct; //Injecting the function created to apply on the value n
     
     @GET
-    @Path("labseq/{n}")
+    @Path("labseq/{n}") //creating the endpoint for n
     @Produces(MediaType.TEXT_PLAIN)
+    //Adding Description for user
     @Operation(
         summary = "calculate labseq(n)",
         description = "Insert value n (n needs to be a non-negative Integer) to get labseq(n)"
     )
     public String hello(int n){
-        long startTime = System.nanoTime();
+        long startTime = System.nanoTime(); //to be used later to calculate execution time performance 
 
+        //check if value has been previously calculated
         if(values_dictionary.get(n)!=null){
-            long endTime   = System.nanoTime();
-            long totalTime = endTime-startTime;
+            long endTime   = System.nanoTime(); //timestamp to use for execution time performance
+            long totalTime = endTime-startTime; //calculating execution time performance
             return "Value for labseq(" + n + ") was already previously calculated:\n" + values_dictionary.get(n) + "\nRuntime to find solution: " + totalTime + " nanoseconds or " + TimeUnit.NANOSECONDS.toSeconds(totalTime) + " seconds.";
+            //returning value for labseq(n) as well has time it took to obtain it in nanoseconds/seconds.
         }
 
+        //check if introduced value of n is a non-negative Integer
         if(n<0){
+            //return error message
             throw new WebApplicationException(Response.status(422).entity("Can't calculate labseq(n)!!\nValue is not a non-negative Integer!!").build());
         }
+        //else call recursive function to obtain the value for labseq(n)
         else{
             BigInteger result_value = labseq_funct.labseq_funct(n,values_dictionary);
-            long endTime   = System.nanoTime();
-            long totalTime = endTime-startTime;
+            long endTime   = System.nanoTime(); //timestamp to use for execution time performance
+            long totalTime = endTime-startTime; //calculating execution time performance
             return "The Result Value for labseq(" + n + ") is:\n" + result_value +"\nRuntime to find solution: " + totalTime + " nanoseconds or " + TimeUnit.NANOSECONDS.toSeconds(totalTime) + " seconds.";
+            //returning value for labseq(n) as well has time it took to obtain it in nanoseconds/seconds.
         }
 
     }
--- src/main/java/Functions.java
+++ src/main/java/Functions.java
@@ -6,21 +6,23 @@ import jakarta.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class Functions {
     public BigInteger labseq_funct(int n,Dictionary<Integer,BigInteger> dict){
+        //check if n belongs to the starter sequence 
         if(n==0||n==2){
             return new BigInteger("0");
         }
         else if(n==1||n==3){
             return new BigInteger("1");
         }
+        //else recursively call function to obtain value for labseq(n)
         else{
-            if (dict.get(n-4)==null){
-                dict.put(n-4, labseq_funct(n-4, dict));
+            if (dict.get(n-4)==null){ //check if value for labseq(n-4) doesn't exist
+                dict.put(n-4, labseq_funct(n-4, dict)); //calculate and add to dictionary value for labseq(n-4)
             }
-            if(dict.get(n-3)==null){
-                dict.put(n-3, labseq_funct(n-3, dict));
+            if(dict.get(n-3)==null){ //check if value for labseq(n-3) doesn't exist
+                dict.put(n-3, labseq_funct(n-3, dict)); //calculate and add to dictionary value for labseq(n-3)
             }
-            dict.put(n,dict.get(n-3).add(dict.get(n-4)));
-            return dict.get(n);
+            dict.put(n,dict.get(n-3).add(dict.get(n-4))); //calculate and add to dictionary value for labseq(n)
+            return dict.get(n); //return value for labseq(n)
         }
     }
 }
